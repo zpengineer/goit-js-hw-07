33daa7e9293cb8b3ccae342458b962b9fe8bf704
zpengineer <clickprime94@gmail.com>
2022-01-06 17:39:25 +0200

    Final homework 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,91 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = creatGalleryItemsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+
+function creatGalleryItemsMarkup(items) {
+
+    return items.map(({original, preview, description}) => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href='${original}'>
+                <img
+                class="gallery__image"
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'
+                />
+            </a>
+        </div>`
+    ).join("");
+    
+}
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const modalMarkup = `<img src="${e.target.dataset.source}" alt="${e.target.alt}">`;
+    const instance = basicLightbox.create(modalMarkup);
+
+    instance.show();
+
+
+    window.addEventListener('keydown', e => {
+         if (e.key === 'Escape') {
+            instance.close();
+        }
+    });
+
+
+
+    window.addEventListener('keydown', onKeyPressArrowLeft);
+    window.addEventListener('keydown', onKeyPressArrowRight);
+    
+}
+
+
+// Дополнительный функционал, перелистывание галлереи с помощью "Стрелки вправо" и "Стрелки влево"
+
+function onKeyPressArrowLeft(e) {  
+
+    const lightBoxRef = document.querySelector('.basicLightbox img');
+
+    if (e.key === 'ArrowLeft') {
+
+        const originalImgArray = galleryItems.map(({ original }) => original);
+
+        let indexCurrentImg = originalImgArray
+            .findIndex(img => img === lightBoxRef.src)
+        
+       lightBoxRef.src = originalImgArray[indexCurrentImg === 0 ? originalImgArray.length - 1 : indexCurrentImg - 1];
+       
+    }
+}
+
+function onKeyPressArrowRight(e) {  
+
+    const lightBoxRef = document.querySelector('.basicLightbox img');
+
+    if (e.key === 'ArrowRight') {
+
+        const originalImgArray = galleryItems.map(({ original }) => original);
+
+        let indexCurrentImg = originalImgArray
+            .findIndex(img => img === lightBoxRef.src);
+
+        lightBoxRef.src = originalImgArray[indexCurrentImg === originalImgArray.length - 1 ? 0 : indexCurrentImg + 1];
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+
+function creatGalleryItemsMarkup(items) {
+    return items.map(({preview, original, description}) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    ).join("");
+}
+
+const galleryItemsMarkup = creatGalleryItemsMarkup(galleryItems);
+galleryContainerRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
